6ed80edfd2b93df801c4d2a43dcc468acf6fa071
ingindIsrael <ing.visrael@gmail.com>
2020-05-05 11:46:25 -0400

    comment deleted

--- lib/contact.dart
+++ lib/contact.dart
@@ -40,7 +40,7 @@ class ProfilePage3 extends StatelessWidget {
     NavButton(
       text: "Contact",
       onPressed: () {
-       // html.window.open("https://pawan.live", "Pk");
+       
         Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => MyApp3())
